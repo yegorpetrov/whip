e9e774e8a8f93edf580c30bc53514b7102377e34
Yegor <petrov.yegor@gmail.com>
2016-04-15 15:48:26 +0500

    Implement some group script methods

---
+++ Whip/Widgets/Group.maki.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whip.Widgets
+{
+    partial class Group
+    {
+        public GuiObject GetObject(string id) => panel.Children
+            .OfType<GuiObject>()
+            .Where(go => go.Id == id)
+            .FirstOrDefault();
+
+        public GuiObject FindObject(string id) => GetObject(id) ??
+            panel
+            .Children
+            .OfType<Group>()
+            .Select(g => g.FindObject(id))
+            .FirstOrDefault(g => g != null);
+
+        public int GetNumObjects() => panel.Children
+            .OfType<GuiObject>()
+            .Count();
+
+        public GuiObject EnumObject(int i) => panel.Children
+            .OfType<GuiObject>()
+            .ElementAt(i);
+    }
+}
