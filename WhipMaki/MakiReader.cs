5639d48b69a690e0f83ced6021e1c87c4f2d259e
Yegor Petrov <petrov.yegor@gmail.com>
2016-04-22 13:19:37 +0500

    Add missing MakiReader

---
+++ WhipMaki/MakiReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WhipMaki
+{
+    class MakiReader : BinaryReader
+    {
+        public MakiReader(Stream s) : base(s) { }
+
+        public override string ReadString() =>
+            Encoding.ASCII.GetString(ReadBytes(ReadUInt16()));
+
+        public Guid ReadGuid() => new Guid(ReadBytes(16));
+
+        public IEnumerable<T> ReadSectionOf<T>(Func<MakiReader, T> f)
+        {
+            for (int i = 0, _i = ReadInt32(); i < _i; i++)
+            {
+                yield return f(this);
+            }
+        }
+    }
+}
