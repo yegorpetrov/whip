a851ae8e2cc6a76ab5ae6bffd87a0b2cf3bb236b
Yegor Petrov <petrov.yegor@gmail.com>
2016-04-26 11:23:31 +0500

    Add disassembler shell

---
+++ mda/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using WhipMaki;
+using WhipMaki.Disassembly;
+
+namespace mda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!args.Any()) Fail("No input file specified");
+            var file = args.Last();
+            if (!File.Exists(file)) Fail(string.Format("The specified file “{0}” doesn't exist", file));
+            var bytes = File.ReadAllBytes(args.Last());
+            var sda = new ScriptDisassembly(new Maki(bytes));
+            sda.Disassemble(null);
+            Console.WriteLine(sda.ToString());
+        }
+
+        static void Fail(string msg)
+        {
+            Console.WriteLine(msg);
+            Environment.Exit(1);
+        }
+    }
+}
